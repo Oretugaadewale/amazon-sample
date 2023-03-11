e0fa8576a98f9b87b94d29d33a569be18d0e0e2c
Oretugaadewale <Oretugafolorunso@gmail.com>
2023-03-11 11:30:45 +0100

    adding files

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img0 from "./images/book0.jpg";
+import img2 from "./images/book2.jpg";
+import img3 from "./images/book3.jpg";
+
+export const books = [
+  {
+    author: "Dr. Mark Hyman MD",
+    title: "Young Forever: The Secrets to Living Your Longest, Healthiest Life",
+    img: img0,
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Jimmy Niro",
+    title: "Dad jokes",
+    img: img3,
+    id: 3,
+  },
+];
